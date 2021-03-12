1bc3bd69dcc69e0868869c3a06cfc4ede05b0033
Paylo01 <paylo_@outlook.com>
2021-03-12 15:49:06 +0100

    installed dependencies and created the layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card, CardBody, Button, Input, InputGroup, InputGroupAddon, Row, Col, List, Container } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col md={12}>
+            <h1>Todo App With React</h1>
+            <Card>
+              <CardBody>
+                <InputGroup>
+                    <Input type="text" placeholder="Add a new task..." />
+                    <InputGroupAddon addonType="prepend"><Button color="primary">Add</Button></InputGroupAddon>
+                </InputGroup>
+                <List type="unstyled">
+                  <li><label><input class="checkbox" type="checkbox" /> Finish 4 hours!</label> <i class="far fa-edit edit"></i><i class="far fa-trash-alt trash"></i></li>
+                  <li><label><input class="checkbox" type="checkbox" /> Finish 4 hours!</label> <i class="far fa-edit edit"></i><i class="far fa-trash-alt trash"></i></li>
+                  <li><label><input class="checkbox" type="checkbox" /> Finish 4 hours!</label> <i class="far fa-edit edit"></i><i class="far fa-trash-alt trash"></i></li>
+                </List>
+              </CardBody>
+          </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
